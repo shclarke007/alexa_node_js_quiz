d53a6afdc65d743f6c9cee90b3529807a057a00b
Adam Lodowski <adamlodowski@gmail.com>
2019-02-20 13:12:52 +0000

    Set up testing framework

---
+++ test/helloworld-tests.js
@@ -0,0 +1,50 @@
+/*
+Mocha tests for the Alexa skill "Hello World" example (https://github.com/alexa/skill-sample-nodejs-hello-world).
+Using the Alexa Skill Test Framework (https://github.com/BrianMacIntosh/alexa-skill-test-framework).
+Run with 'mocha examples/skill-sample-nodejs-hello-world/helloworld-tests.js'.
+*/
+
+// include the testing framework
+const alexaTest = require('alexa-skill-test-framework');
+// const alexaTest = require('../../index');
+
+// initialize the testing framework
+alexaTest.initialize(
+  require('../lambda/custom/index.js'),
+	"amzn1.ask.skill.00000000-0000-0000-0000-000000000000",
+	"amzn1.ask.account.VOID");
+
+describe("Hello World Skill", function () {
+	// tests the behavior of the skill's LaunchRequest
+	describe("LaunchRequest", function () {
+		alexaTest.test([
+			{
+				request: alexaTest.getLaunchRequest(),
+				says: "Hello World!", repromptsNothing: true, shouldEndSession: true
+			}
+		]);
+	});
+
+	// tests the behavior of the skill's HelloWorldIntent
+	describe("HelloWorldIntent", function () {
+		alexaTest.test([
+			{
+				request: alexaTest.getIntentRequest("HelloWorldIntent"),
+				says: "Hello World!", repromptsNothing: true, shouldEndSession: true,
+				hasAttributes: {
+					foo: 'bar'
+				}
+			}
+		]);
+	});
+
+	// tests the behavior of the skill's HelloWorldIntent with like operator
+	describe("HelloWorldIntent like", function () {
+		alexaTest.test([
+			{
+				request: alexaTest.getIntentRequest("HelloWorldIntent"),
+				saysLike: "World", repromptsNothing: true, shouldEndSession: true
+			}
+		]);
+	});
+});
