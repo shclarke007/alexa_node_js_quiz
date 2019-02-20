90f5f023cbc3646fac87bdecedefcc95ff687e57
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-20 14:12:48 +0000

    removed index.js template

--- test/helloworld-tests.js
+++ test/helloworld-tests.js
@@ -1,5 +1,5 @@
 /*
-Mocha tests for the Alexa skill "Hello World" example (https://github.com/alexa/skill-sample-nodejs-hello-world).
+Mocha tests for the Alexa skill "Quizza" example (https://github.com/alexa/skill-sample-nodejs-hello-world).
 Using the Alexa Skill Test Framework (https://github.com/BrianMacIntosh/alexa-skill-test-framework).
 Run with 'mocha examples/skill-sample-nodejs-hello-world/helloworld-tests.js'.
 */
@@ -14,7 +14,7 @@ alexaTest.initialize(
 	"amzn1.ask.skill.00000000-0000-0000-0000-000000000000",
 	"amzn1.ask.account.VOID");
 
-describe("Hello World Skill", function () {
+describe("Quizza Skill", function () {
 	// tests the behavior of the skill's LaunchRequest
 	describe("LaunchRequest", function () {
 		alexaTest.test([
