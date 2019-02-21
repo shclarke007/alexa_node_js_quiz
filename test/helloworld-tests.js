d8e50096f013ed193f1dbd0b893edcd5e0e0f396
Carol Gardiner <carolan.gardiner@gmail.com>
2019-02-21 20:43:05 +0000

    add unit test for transition

--- test/helloworld-tests.js
+++ test/helloworld-tests.js
@@ -5,7 +5,15 @@ alexaTest.initialize(
 	'amzn1.ask.skill.00000000-0000-0000-0000-000000000000',
 	'amzn1.ask.account.VOID')
 
+	
+
 describe('Quizza Skill', function () {
+	//tests the locale of the framework can change
+	describe('locale', function (){
+		alexaTest.setLocale('en-GB')
+	})
+
+	
 	// tests the behavior of the skill's LaunchRequest
 	describe('LaunchRequest', function () {
 		alexaTest.test([
@@ -15,7 +23,23 @@ describe('Quizza Skill', function () {
 			}
 		]);
 	});
-  
+	
+	//tests the behaviour of th skills Ready Intent
+	describe('ReadyIntent', function (){
+		const slot = {'YesType': 'yes'}
+		const requestWithSlot = alexaTest.getIntentRequest('ReadyIntent', slot);
+		alexaTest.test([ 
+			{
+			request: requestWithSlot,
+			saysLike: "Ok, let\'s get started", 
+			repromptsNothing: false, 
+			shouldEndSession: false
+
+			}
+		])
+	});
+	
+	//tests skill  StartOver Intent
 	describe("StartOverRequest", function () {
 		alexaTest.test([
 			{
@@ -24,14 +48,31 @@ describe('Quizza Skill', function () {
 			}
 		]);
 	});
-  
+
+	describe("StartOverIntent into StopIntent", function(){
+		alexaTest.test([
+			{ 
+				request: alexaTest.getIntentRequest("AMAZON.StartOverIntent"),
+				 says: 'Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Are you ready?', 
+				 shouldEndSession: false 
+			},
+			{ 
+				request: alexaTest.getIntentRequest("AMAZON.StopIntent"), 
+				says: "Goodbye", shouldEndSession: true 
+			}
+		]);
+	});
+	
+
 	describe("StopIntent", function () {
 		alexaTest.test([
 			{
 				request: alexaTest.getIntentRequest('AMAZON.StopIntent'),
-				says: 'Goodbye', repromptsNothing: true, shouldEndSession: true
+				saysLike: 'Goodbye', repromptsNothing: true, shouldEndSession: true
 			}
 		]);
 	});
-  
+
+	
+  describe
 });
