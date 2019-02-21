96c762e343fbc596c0bbe07f8820ac50eb1574b3
Carol Gardiner <carolan.gardiner@gmail.com>
2019-02-21 09:20:24 +0000

    added answer intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,5 +1,38 @@
 const Alexa = require('ask-sdk-core');
 
+
+const AnswerIntent_Handler =  {
+  canHandle(handlerInput) {
+      const request = handlerInput.requestEnvelope.request;
+      return request.type === 'IntentRequest' && request.intent.name === 'AnswerIntent' ;
+  },
+  
+  handle(handlerInput) {
+      const request = handlerInput.requestEnvelope.request;
+      const responseBuilder = handlerInput.responseBuilder;
+      let sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+     
+
+      let slotStatus = '';
+      let resolvedSlot;
+
+      let slotValues = getSlotValues(request.intent.slots); 
+      // getSlotValues returns .heardAs, .resolved, and .isValidated for each slot, according to request slot status codes ER_SUCCESS_MATCH, ER_SUCCESS_NO_MATCH, or traditional simple request slot without resolutions
+          
+          /* slotValue = {
+              answer: {
+                heardAs: 
+              }
+         */ 
+      let say = `I heard the following answer: ${slotValues.answer.heardAs} `;
+      return responseBuilder
+          .speak(say)
+          .reprompt('try again, ' + say)
+          .getResponse();
+  },
+};
+
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type == 'LaunchRequest';
@@ -11,9 +44,12 @@ const LaunchRequestHandler = {
   },
 };
 
+
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
-    LaunchRequestHandler
+    LaunchRequestHandler,
+    AnswerIntent_Handler
   )
 .lambda()
