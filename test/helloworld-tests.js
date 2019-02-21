7c7843e0c4fd0d76ae12ca57d9ff174cc149ef06
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-21 14:44:03 +0000

    added start over intent

--- test/helloworld-tests.js
+++ test/helloworld-tests.js
@@ -1,14 +1,5 @@
-/*
-Mocha tests for the Alexa skill "Quizza" example (https://github.com/alexa/skill-sample-nodejs-hello-world).
-Using the Alexa Skill Test Framework (https://github.com/BrianMacIntosh/alexa-skill-test-framework).
-Run with 'mocha examples/skill-sample-nodejs-hello-world/helloworld-tests.js'.
-*/
-
-// include the testing framework
 const alexaTest = require('alexa-skill-test-framework');
-// const alexaTest = require('../../index');
 
-// initialize the testing framework
 alexaTest.initialize(
   require('../lambda/custom/index.js'),
 	"amzn1.ask.skill.00000000-0000-0000-0000-000000000000",
@@ -20,8 +11,17 @@ describe("Quizza Skill", function () {
 		alexaTest.test([
 			{
 				request: alexaTest.getLaunchRequest(),
-				says: "Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Good Luck!", repromptsNothing: true, shouldEndSession: true
+				saysLike: 'Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Are you ready?', repromptsNothing: false, shouldEndSession: false
 			}
 		]);
 	});
+	describe("StartOverRequest", function () {
+		alexaTest.test([
+			{
+				request: alexaTest.getLaunchRequest(),
+				saysLike: 'Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Are you ready?', repromptsNothing: false, shouldEndSession: false
+			}
+		]);
+	});
+		
 });
