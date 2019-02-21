35170b71ca7ab1d7694ccba28e9faaec4423d109
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-21 15:25:15 +0000

    fixed linting issues

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -4,48 +4,48 @@ const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type == 'LaunchRequest'
     || handlerInput.requestEnvelope.request.type === 'IntentRequest'
-    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent';
+    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
     .speak('Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Are you ready?')
     .withShouldEndSession (false)
-    .getResponse();
+    .getResponse()
   },
-};
+}
 
 const ReadyIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent'
   },
   handle(handlerInput) {
-    const slots = handlerInput.requestEnvelope.request.intent.slots;
-    const yesAnswer = slots['yes'].value;
-    const noAnswer = slots['no'].value;
+    const slots = handlerInput.requestEnvelope.request.intent.slots
+    const yesAnswer = slots['yes'].value
+    const noAnswer = slots['no'].value
   
   if (yesAnswer == 'yes') {
-    var speechText = "Ok, let's get started";
+    var speechText = 'Ok, let\'s get started'
     return handlerInput.responseBuilder
       .speak(speechText)
       .withShouldEndSession (false)
-      .getResponse();
+      .getResponse()
     } 
   if (noAnswer == 'no') {
-    speechText = "Well I don't have all day so hurry up.";
+    speechText = 'Well I don\'t have all day so hurry up.'
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt('Are you ready yet?')
       .withShouldEndSession (false)
-      .getResponse();
+      .getResponse()
   }
   },
-};
+}
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const skillBuilder = Alexa.SkillBuilders.custom()
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     ReadyIntentHandler
   )
-.lambda();
+.lambda()
