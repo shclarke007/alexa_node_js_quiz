559319a8e8dd1c1d259cbed984181bd79426f775
Adam Lodowski <adamlodowski@gmail.com>
2019-02-21 14:32:47 +0000

    Fixed linting errors and created .eslintignore for nodemodules

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,17 +1,17 @@
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk-core')
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type == 'LaunchRequest';
+    return handlerInput.requestEnvelope.request.type == 'LaunchRequest'
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-    .speak("Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Good Luck!")
-    .getResponse();
+    .speak('Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Good Luck!')
+    .getResponse()
   },
-};
+}
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const skillBuilder = Alexa.SkillBuilders.custom()
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler
