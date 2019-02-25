19d9f971521fe3af64b3f11141ea9ba988e6f85d
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2019-02-25 16:23:43 +0000

    Error handler

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,65 +1,184 @@
-const Alexa = require('ask-sdk-core')
+const Alexa = require('ask-sdk-core');
+const questions = require('./questions');
+
+const welcomeMessage = 'Welcome to Quizza. A game with questions based on the popular life in the UK test. Are you ready to find out if you are truly British?';
+const goodbyeMessage = 'Goodbye. Thanks for playing.';
+const readyMessage = 'Ok. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answers or passed questions. Say start game to hear your first question. When ready to answer just say the answer';
+const readyReprompt = 'Say start to hear your first question. When ready to answer just say the answer';
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
     || handlerInput.requestEnvelope.request.type === 'IntentRequest'
-    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
+    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent';
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-    .speak('Welcome to Quizza. Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answer or passed questions. Are you ready?')
+    .speak(`<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_intro_01'/> ${welcomeMessage}`)
     .withShouldEndSession (false)
-    .getResponse()
+    .getResponse();
   },
-}
-
-const StopIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-    .speak('Goodbye')
-    .getResponse()
-  }
-}
+};
 
 const ReadyIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent'
+      && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent';
   },
   handle(handlerInput) {
-    const slots = handlerInput.requestEnvelope.request.intent.slots
-    const yesAnswer = slots['yes'].value
-    const noAnswer = slots['no'].value
-  
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const yesAnswer = slots['yes'].value;
+    const noAnswer = slots['no'].value;
+    var speechText = "";
   if (yesAnswer === 'yes') {
-    var speechText = 'Ok, let\'s get started'
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withShouldEndSession (false)
-      .getResponse()
+    console.log("Does this work?")
+    speechText = `${readyMessage} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_waiting_loop_30s_01'/>`;
     } 
-  if (noAnswer === 'no') {
-    speechText = 'Well I don\'t have all day so hurry up.'
+    if (noAnswer === 'no') {
+      speechText =`Well I don't have all day. Hurry up. <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01'/>`;
+    }
     return handlerInput.responseBuilder
       .speak(speechText)
-      .reprompt('Are you ready yet?')
+      .reprompt(readyReprompt)
       .withShouldEndSession (false)
-      .getResponse()
-  }
+      .getResponse();
   },
+};
+
+const StartQuizIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'QuizIntent';
+  },
+  handle(handlerInput) {
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    var quiz_questions = choose_questions(questions.QUESTIONS_EN_GB);
+    sessionAttributes.allQuestions = quiz_questions;
+    var currentQuestionObject = sessionAttributes.allQuestions.pop();
+    var currentQuestion = currentQuestionObject.question;  
+    sessionAttributes.question = currentQuestion;
+    var currentAnswer = currentQuestionObject.answer;
+    sessionAttributes.answer = currentAnswer;
+    sessionAttributes.score = 0; 
+   
+    return handlerInput.responseBuilder
+      .speak(`${currentQuestion} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01'/>`)
+      .reprompt(currentQuestion)
+      .withShouldEndSession (false)
+      .getResponse();
+  },
+};
+
+const AnswerIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AnswerIntent';
+  },
+  handle(handlerInput) {
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    const slot = handlerInput.requestEnvelope.request.intent.slots;
+    const answerSlot = slot['answer'].value.toLowerCase();
+    console.log(answerSlot);
+    var speechText = ``;
+    speechText+= determine_correct(answerSlot, sessionAttributes.answer, handlerInput);
+    speechText+= getNextQuestion(handlerInput)
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withShouldEndSession (false)
+      .getResponse();
+      
+    
+  },
+};
+
+const StopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+    .speak(`<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_outro_01'/> ${goodbyeMessage}`)
+    .getResponse();
+  }
+};
+
+function determine_correct(answer_slot, session_attribute, handlerInput) {
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    if (answer_slot === session_attribute) {
+        sessionAttributes.score ++
+      return `<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01'/> correct. One point has been added to your score. `;
+    } else {
+      return `<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_negative_01'/> sorry, that is wrong. `;  
+    }
 }
 
-const skillBuilder = Alexa.SkillBuilders.custom()
+function choose_questions(questions_doc) {
+    var shuffled_array = shuffle_questions(questions_doc).slice(-5)
+    return shuffled_array
+}
 
+function shuffle_questions(array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+function getNextQuestion(handlerInput) {
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    var currentQuestionObject = sessionAttributes.allQuestions.pop();
+    var currentQuestion = currentQuestionObject.question;  
+    sessionAttributes.question = currentQuestion;
+    var currentAnswer = currentQuestionObject.answer;
+    sessionAttributes.answer = currentAnswer;
+   
+   if(sessionAttributes.allQuestions.length > 0) {
+    return `The next question is: ${currentQuestion}`
+   } else {
+    return `You scored ${sessionAttributes.score}. Thank you for playing`
+   }
+}
+
+const SKILL_NAME = 'Quizz world';
+
+const FALLBACK_MESSAGE = `${SKILL_NAME} skill can't help you with that. However, brexit means brexit`;
+
+const FALLBACK_Reprompt = 'What can I help you with'
+
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('I do not understand that command')
+      .reprompt('What would like to do now?')
+      .withShouldEndSession(false)
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     ReadyIntentHandler,
-    StopIntentHandler
+    StartQuizIntentHandler,
+    AnswerIntentHandler,
+    StopIntentHandler,
   )
-.lambda()
+.addErrorHandlers(ErrorHandler)
+.lambda();
