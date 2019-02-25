fa5651662507b32003318ffffb90d4c2298a0140
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2019-02-25 16:43:06 +0000

    Added HelperHandler

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -160,12 +160,31 @@ const ErrorHandler = {
   },
 }
 
+const helpMessage = 'Here are the rules. I will ask you five questions in round one. For every correct answer you will get one point added to your score. You can pass if you do not know the answer. No point will be awarded for incorrect answers or passed questions. To answer a question, say a, b, or c. Say start game to hear your questions'
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent'
+  },
+  handle(handlerInput) {
+    const speechText = `${helpMessage}`
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withShouldEndSession(false)
+      .getResponse()
+  },
+}
+
 const skillBuilder = Alexa.SkillBuilders.custom()
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     ReadyIntentHandler,
     StartQuizIntentHandler,
+    HelpIntentHandler,
     AnswerIntentHandler,
     StopIntentHandler,
   )
